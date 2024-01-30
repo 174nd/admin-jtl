be4024d15fe600541ee22d8c187ab7eedf8a01cd
174nd <andilewispratama11@gmail.com>
2024-01-31 04:38:58 +0700

    x

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/Input";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Login() {
   return (
@@ -22,12 +23,12 @@ export default function Login() {
             <Input props={{className: "w-full", type: "email", placeholder: "email"}}/>
             <Input props={{className: "w-full", type: "password", placeholder: "password"}}/>
             
-            <button className="bg-blue-500 hover:bg-blue-600 rounded-xl w-full text-white py-2 hover:scale-105 duration-300">
+            <Link href="/dashboard" className="bg-blue-500 hover:bg-blue-600 rounded-xl w-full text-white py-2 hover:scale-105 duration-300">
               <svg className="w-5 h-5 inline"fill="none"stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
               </svg>
               Login
-            </button>
+            </Link>
           </form>
         </div>
       </div>
